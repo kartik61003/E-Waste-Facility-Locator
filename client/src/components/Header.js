ff2501bd4b5f70a51adb34d1747b217680210dd9
kartik61003 <kartik61003@gmail.com>
2023-09-23 00:38:50 +0530

    BACKGROUND CREATED + FEW Visual IMPROVEMTN

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,60 @@
 import React from 'react'
 
-const Header = () => {
+// const Header = () => {
+//   return (
+//     <>
+//       <div className='navbar'>
+//         <div className='app-logo'>
+//           <img id='navbar-image' src="https://pics.clipartpng.com/Renewable_Energy_PNG_Clipart-2976.png" alt=""></img>
+//         </div>
+//         <div className='btn-grp'>            
+//         <a>Estimate Calculator</a>
+//         <a> CONTACT US</a>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+import { useState, useEffect } from "react";
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <div className='navbar'>
-        <div className='app-logo'>
-          <img id='navbar-image' src="https://pics.clipartpng.com/Renewable_Energy_PNG_Clipart-2976.png" alt=""></img>
-        </div>
-        <div className='btn-grp'>            
-        <a>Estimate Calculator</a>
-        <a> CONTACT US</a>
-        </div>
-      </div>
-    </>
-  )
+    <div className={`navbar ${isScrolled ? "scrolled" : ""}`}>
+      {
+        <>
+          <div className='navbar'>
+            <div className='app-logo'>
+              <img id='navbar-image' src="https://pics.clipartpng.com/Renewable_Energy_PNG_Clipart-2976.png" alt=""></img>
+            </div>
+            <div className='btn-grp'>            
+            <a>Estimate Calculator</a>
+            <a> CONTACT US</a>
+            </div>
+          </div>
+        </>
+      }
+    </div>
+  );
 }
+
 
 export default Header
