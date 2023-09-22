80e190aa8cd0ca25bbb2fd8df390409abe6a645f
kartik61003 <kartik61003@gmail.com>
2023-09-23 01:57:52 +0530

    CARD LAYOUT MADE

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TypewriterEffect from '../components/TypewriterEffect'
+import Cards from '../components/Cards'
 
 const Index = () => {
   return (
@@ -16,8 +17,17 @@ const Index = () => {
         Our dedicated team has created this user-friendly website to simplify your electronic waste recycling journey. We understand the importance of responsible e-waste disposal and its impact on the environment. With our platform, you can effortlessly find nearby e-waste facilities, whether it's a recycling center, drop-off point, or upcoming collection event. But that's not all; we go beyond just locating facilities. You can also calculate and visualize the positive environmental footprint you're leaving behind through your recycling efforts. Dive into our community of like-minded individuals and organizations who are passionate about reducing e-waste. Stay informed about the latest innovations and trends in the e-waste recycling sector. Join us today in our mission to safeguard the planet, conserve valuable resources, and create a sustainable future for generations to come. Start by locating your nearest recycling center and tracking your impactful journey toward a greener world. Together, one electronic device at a time, we can make a lasting difference. 
         </p>
         </div>
-
       </div>
+
+      <div className='cards-container'>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+
+        </div>
     </>
   )
 }
